29206bc3361cdaf6425629219544727671e71536
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-26 20:41:52 -0300

    Update EmployeeService.py

--- python/EmployeeService.py
+++ python/EmployeeService.py
@@ -7,73 +7,93 @@ import EmployeeService_pb2_grpc
 
 import const
 
-empDB=[
- {
- 'id':101,
- 'name':'Saravanan S',
- 'title':'Technical Leader',
- 'salary': 12500.0
- },
- {
- 'id':201,
- 'name':'Rajkumar P',
- 'title':'Sr Software Engineer',
- 'salary': 9800.0
- }
- ]
+empDB = [
+    {
+        'id': 101,
+        'name': 'Saravanan S',
+        'title': 'Technical Leader',
+        'salary': 12500.0
+    },
+    {
+        'id': 201,
+        'name': 'Rajkumar P',
+        'title': 'Sr Software Engineer',
+        'salary': 9800.0
+    }
+]
 
 class EmployeeServer(EmployeeService_pb2_grpc.EmployeeServiceServicer):
 
-def UpdateEmployeeSalary(self, request, context):
-    usr = [emp for emp in empDB if emp['id'] == request.id]
+    def UpdateEmployeeSalary(self, request, context):
+        usr = [emp for emp in empDB if emp['id'] == request.id]
 
-    if len(usr) == 0:
-        return EmployeeService_pb2.StatusReply(status='NOK')
+        if len(usr) == 0:
+            return EmployeeService_pb2.StatusReply(status='NOK')
 
-    usr[0]['salary'] = request.salary
-    return EmployeeService_pb2.StatusReply(status='OK')
-   
- 
-  def CreateEmployee(self, request, context):
-    dat = {
-    'id':request.id,
-    'name':request.name,
-    'title':request.title
-    }
-    empDB.append(dat)
-    return EmployeeService_pb2.StatusReply(status='OK')
+        usr[0]['salary'] = request.salary
+        return EmployeeService_pb2.StatusReply(status='OK')
 
-  def GetEmployeeDataFromID(self, request, context):
-    usr = [ emp for emp in empDB if (emp['id'] == request.id) ] 
-    return EmployeeService_pb2.EmployeeData(id=usr[0]['id'], name=usr[0]['name'], title=usr[0]['title'])
+    def CreateEmployee(self, request, context):
+        dat = {
+            'id': request.id,
+            'name': request.name,
+            'title': request.title,
+            'salary': request.salary
+        }
+        empDB.append(dat)
+        return EmployeeService_pb2.StatusReply(status='OK')
 
-  def UpdateEmployeeTitle(self, request, context):
-    usr = [ emp for emp in empDB if (emp['id'] == request.id) ]
-    usr[0]['title'] = request.title
-    return EmployeeService_pb2.StatusReply(status='OK')
+    def GetEmployeeDataFromID(self, request, context):
+        usr = [emp for emp in empDB if emp['id'] == request.id]
 
-  def DeleteEmployee(self, request, context):
-    usr = [ emp for emp in empDB if (emp['id'] == request.id) ]
-    if len(usr) == 0:
-      return EmployeeService_pb2.StatusReply(status='NOK')
+        if len(usr) == 0:
+            return EmployeeService_pb2.EmployeeData()
 
-    empDB.remove(usr[0])
-    return EmployeeService_pb2.StatusReply(status='OK')
+        return EmployeeService_pb2.EmployeeData(
+            id=usr[0]['id'],
+            name=usr[0]['name'],
+            title=usr[0]['title'],
+            salary=usr[0]['salary']
+        )
 
-  def ListAllEmployees(self, request, context):
-    list = EmployeeService_pb2.EmployeeDataList()
-    for item in empDB:
-      emp_data = EmployeeService_pb2.EmployeeData(id=item['id'], name=item['name'], title=item['title']) 
-      list.employee_data.append(emp_data)
-    return list
+    def UpdateEmployeeTitle(self, request, context):
+        usr = [emp for emp in empDB if emp['id'] == request.id]
+
+        if len(usr) == 0:
+            return EmployeeService_pb2.StatusReply(status='NOK')
+
+        usr[0]['title'] = request.title
+        return EmployeeService_pb2.StatusReply(status='OK')
+
+    def DeleteEmployee(self, request, context):
+        usr = [emp for emp in empDB if emp['id'] == request.id]
+
+        if len(usr) == 0:
+            return EmployeeService_pb2.StatusReply(status='NOK')
+
+        empDB.remove(usr[0])
+        return EmployeeService_pb2.StatusReply(status='OK')
+
+    def ListAllEmployees(self, request, context):
+        lista = EmployeeService_pb2.EmployeeDataList()
+
+        for item in empDB:
+            emp_data = EmployeeService_pb2.EmployeeData(
+                id=item['id'],
+                name=item['name'],
+                title=item['title'],
+                salary=item['salary']
+            )
+            lista.employee_data.append(emp_data)
+
+        return lista
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     EmployeeService_pb2_grpc.add_EmployeeServiceServicer_to_server(EmployeeServer(), server)
-    server.add_insecure_port('[::]:'+const.PORT)
+    server.add_insecure_port('[::]:' + const.PORT)
     server.start()
     server.wait_for_termination()
-
 
 if __name__ == '__main__':
     logging.basicConfig()
